01be21bd050add18c3e8e7afea98f1d6555349aa
Bahman <bahman@Bahmans-MacBook-Air.local>
2025-01-12 09:49:30 +0330

    second commit

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -1,21 +1,18 @@
 import { createReadStream, statSync } from "fs";
 import { NextRequest, NextResponse } from "next/server";
 
-const music = 'music.mp3'
-
-const stat = statSync(music);
+// export const runtime = "edge";
 
 const handler = async (req: NextRequest) => {
-
+  const music = "music.mp3";
+  const stat = statSync(music);
   const stream = createReadStream(music);
-
   return new NextResponse(stream as any, {
     headers: {
       "content-type": "audio/mpeg",
-      "content-length": stat.size.toString()
+      "content-length": stat.size.toString(),
     },
   });
 };
 
-
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST };
